8872e78e8b6e82276721a5ee46572e45230449c3
nicoleables <159223166+nicoleables@users.noreply.github.com>
2024-07-31 11:24:11 -0500

    fixed helpers

--- Develop/server.js
+++ Develop/server.js
@@ -6,7 +6,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
-const helpers = require('./helpers/ifEquals');
+const helpers = require('./helpers/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
